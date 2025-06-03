c43d2bb7c4e91056a808fa34795b5279b53c4448
Ian Ramy <imwagore@gmail.com>
2025-06-03 11:29:47 +0300

    Updated the pages/

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,29 +1,29 @@
 // alx-project-0x02/pages/about.tsx
 
-import Header from "../components/layout/Header";
-import Button from "../components/common/Button";
+import Header from "@/components/layout/Header";
+import Button from "@/components/common/Button";
 
 export default function About() {
-	return (
-		<>
-			<Header />
-			<div className="p-4 space-x-4">
-				<Button
-					size="small"
-					shape="rounded-sm">
-					Small
-				</Button>
-				<Button
-					size="medium"
-					shape="rounded-md">
-					Medium
-				</Button>
-				<Button
-					size="large"
-					shape="rounded-full">
-					Large
-				</Button>
-			</div>
-		</>
-	);
+    return (
+        <>
+            <Header />
+            <div className="p-4 space-x-4">
+                <Button
+                    size="small"
+                    shape="rounded-sm">
+                    Small
+                </Button>
+                <Button
+                    size="medium"
+                    shape="rounded-md">
+                    Medium
+                </Button>
+                <Button
+                    size="large"
+                    shape="rounded-full">
+                    Large
+                </Button>
+            </div>
+        </>
+    );
 }
